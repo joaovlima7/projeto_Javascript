1640b4833d88092dae3f08551dec902ee5ce9b71
João Victor <joaovlima775@gmail.com>
2022-04-20 16:14:21 -0300

    aula javascript

--- aula04_atividade/js/script.js
+++ aula04_atividade/js/script.js
@@ -1,6 +1,22 @@
-let nome = 'João'
-let idade = '18'
-let peso = '52'
-let altura = '1.70'
-let cpf = '134.567.890-12'
-let rg = '32.345.789-3'
+/**
+Aluno: João Victor
+Matéria: Javascript
+Variáveis - Atividade 01 
+Data: 20/04/2022
+**/
+
+//Declaração
+let nome = 'João Victor'
+let idade = '18 anos'
+let peso = '52 kg'
+let altura = '1,70'
+let cpf = '135.689.742-77' 
+let rg = '98.753.142-7'
+
+//Saída
+document.write(`Nome: ${nome}<br>`)
+document.write(`Idade: ${idade}<br>`)
+document.write(`Peso: ${peso}<br>`)
+document.write(`Altura: ${altura}<br>`)
+document.write(`CPF: ${cpf}<br>`)
+document.write(`RG: ${rg}<br>`)
